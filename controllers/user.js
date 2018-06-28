cb26d404065792e9a1ac33b3ea1cfa316695b24b
peng zhifang <1318255539@qq.com>
2018-06-29 00:48:50 +0800

    登录功能实现

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,42 @@
 const db = require('./db_helper');
+const md5 = require('md5');
 
 exports.showSignin = (req, res) => {
    res.render('signin.html');
 }
 exports.handleSignin = (req, res) => {
    // res.render('signin.html');
-   
+   //验证邮箱是否正确
+   db.query(
+            'select * from `users` where `email`=?',
+            req.body.email,
+            (err, results) => {
+               if (err) {
+                  return res.json({
+                     code: 500,
+                     message: '服务器内部错误'
+                  });
+               }
+               if (results.length <= 0) {
+                  return res.json({
+                     code: 401,
+                     message: '邮箱不存在'
+                  })
+               }
+
+               //验证密码是否正确
+               const password = md5(req.body.password);
+               if (password != results[0].password) {
+                  return res.json({
+                     code: 402,
+                     message: '密码错误'
+                  })
+               }
+               res.json({
+                  code: 200,
+                  message: '登录成功'
+               })
+            })
 }
 exports.showSignup = (req, res) => {
    res.render('signup.html');
