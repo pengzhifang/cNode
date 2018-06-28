80237f8b69bd8f65c70b6af4b1e6d87f9b57912c
peng zhifang <1318255539@qq.com>
2018-06-28 21:38:18 +0800

    注册功能实现

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const art = require('express-art-template');
 const router = require('./routes/router');
+const bodyParser = require('body-parser');
 
 const app = new express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.engine('html', art);
 app.use('/public', express.static('./public'));
--- controllers/user.js
+++ controllers/user.js
@@ -1,15 +1,38 @@
+const db = require('./db_helper');
+
 exports.showSignin = (req, res) => {
    res.render('signin.html');
 }
 exports.handleSignin = (req, res) => {
-   res.send();
+   res.render('signin.html');
 }
 exports.showSignup = (req, res) => {
    res.render('signup.html');
 }
 exports.handleSignup = (req, res) => {
-   res.send();
+   
+            
+   req.body.createdAt = new Date();
+   db.query('insert into `users` set ?', 
+            req.body,
+            (err, results) => {
+               if (err) {
+                  console.log(err);
+                  return res.send('服务器内部错误');
+               }
+               // console.log(results);
+               if (results.affectedRows === 1) {
+                  res.redirect('/signin');
+                  
+               } else {
+                  res.render('signup.html', {
+                     msg: "注册失败"
+                  })
+               }
+    });
 }
 exports.handleSignout = (req, res) => {
    res.send();
 }
+
+ 
