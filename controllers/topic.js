2b3d6b54855e3b49c054e675fa9f821f95a02048
peng zhifang <1318255539@qq.com>
2018-06-29 22:00:02 +0800

    配置话题路由

---
+++ controllers/topic.js
@@ -0,0 +1,15 @@
+exports.showCreate = (req, res) => {
+   res.render('topic/create.html');
+}
+exports.handleCreate = (req, res) => {
+   res.send('handleCreate');
+}
+exports.showTopic = (req, res) => {
+   res.send('showTopic');
+}
+exports.showEdit = (req, res) => {
+   res.send('showEdit');
+}
+exports.handleEdit = (req, res) => {
+   res.send('handleEdit');
+}
--- routes/router.js
+++ routes/router.js
@@ -8,9 +8,19 @@ const topicCtrl = require('../controllers/topic');
 
 module.exports = router;
 
-router.get('/', indexCtrl.showIndex)
-      .get('/signin', userCtrl.showSignin)
+//首页路由
+router.get('/', indexCtrl.showIndex);
+
+//用户路由
+router.get('/signin', userCtrl.showSignin)
       .post('/signin', userCtrl.handleSignin)
       .get('/signup', userCtrl.showSignup)
       .post('/signup', userCtrl.handleSignup)
       .get('/signout', userCtrl.handleSignout);
+
+//话题路由
+router.get('/topic/create', topicCtrl.showCreate)
+      .post('/topic/create', topicCtrl.handleCreate)
+      .get('/topic/:topicID', topicCtrl.showTopic)
+      .get('/topic/:topicID/edit', topicCtrl.showEdit)
+      .post('/topic/:topicID/edit', topicCtrl.handleEdit)
