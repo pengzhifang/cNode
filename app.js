c20fbd0042de7734f9547bb4db15ef58a0116723
peng zhifang <1318255539@qq.com>
2018-06-29 21:31:15 +0800

    将session数据持久化存储到数据库

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const art = require('express-art-template');
 const router = require('./routes/router');
 const bodyParser = require('body-parser');
 const session = require('express-session');
+const MySQLStore = require('express-mysql-session')(session);
+
 
 const app = new express();
 
@@ -16,10 +18,22 @@ app.engine('html', art);
 app.use('/public', express.static('./public'));
 app.use('/node_modules', express.static('./node_modules'));
 
+var options = {
+	host: 'localhost',
+	port: 3306,
+	user: 'root',
+	password: 'root',
+	database: 'ithub'
+};
+
+var sessionStore = new MySQLStore(options);
+
 app.use(session({
-   secret: 'keyboard cat',
-   resave: false,
-   saveUninitialized: true
+   key: 'session_id',  //修改session的名称
+   secret: 'keyboard cat',  //对sessionid进行加密
+   store: sessionStore,     //配置把session数据存储到MySQL数据库中
+   resave: false,           //强制重新存储服务器上的session数据
+   saveUninitialized: true  //默认即使不写session也会生成sessionid;
 }))
 
 const PORT = 3000;
