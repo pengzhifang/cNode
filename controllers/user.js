6ff6266146590640d7d49eff28f331de4f8ce8e1
peng zhifang <1318255539@qq.com>
2018-06-29 20:06:32 +0800

    登录功能修改完成

--- controllers/user.js
+++ controllers/user.js
@@ -4,43 +4,41 @@ const md5 = require('md5');
 exports.showSignin = (req, res) => {
    res.render('signin.html');
 }
+
+//处理登录页面
 exports.handleSignin = (req, res) => {
    // res.render('signin.html');
    //验证邮箱是否正确
-   db.query(
-            'select * from `users` where `email`=?',
-            req.body.email,
-            (err, results) => {
-               if (err) {
-                  return res.json({
-                     code: 500,
-                     message: '服务器内部错误'
-                  });
-               }
-               if (results.length <= 0) {
-                  return res.json({
-                     code: 401,
-                     message: '邮箱不存在'
-                  })
-               }
-
-               //验证密码是否正确
-               const password = md5(req.body.password);
-               if (password != results[0].password) {
-                  return res.json({
-                     code: 402,
-                     message: '密码错误'
-                  })
-               }
-               res.json({
-                  code: 200,
-                  message: '登录成功'
-               })
-            })
+   userModel.getByEmail(req.body.email, (err, user) => {
+      if (err) {
+         return res.send('服务器内部错误');
+      }
+      if (!user) {
+         return res.json({
+            code: 401,
+            message: '邮箱不存在'
+         })
+      }
+      //验证密码是否正确
+      const password = md5(req.body.password);
+      if (password === user.password) {
+         res.json({
+            code: 200,
+            message: '登录成功'
+         })
+      } else {
+         return res.json({
+            code: 402,
+            message: '密码错误,请重新输入'
+         })
+      }
+   })
 }
 exports.showSignup = (req, res) => {
    res.render('signup.html');
 }
+
+//处理注册页面
 exports.handleSignup = (req, res) => {
    //验证邮箱是否存在
    // console.log(req.body);
