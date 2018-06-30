6e9039ad32c9897769963c2da66e60b11b6b0058
peng zhifang <1318255539@qq.com>
2018-06-30 13:51:59 +0800

    添加话题的接口

--- controllers/topic.js
+++ controllers/topic.js
@@ -1,4 +1,5 @@
 const categoryModel = require('../models/category');
+const topicModel = require('../models/topic');
 
 exports.showCreate = (req, res) => {
    categoryModel.getAll((err, categories) => {
@@ -10,8 +11,36 @@ exports.showCreate = (req, res) => {
       });
    })
 }
+
+//处理发布话题
 exports.handleCreate = (req, res) => {
-   res.send('handleCreate');
+   if (!req.session.user) {
+      res.json({
+         code: 403,
+         msg: '登录过期,请先登录'
+      })
+   }
+   req.body.userId = req.session.user.id;
+   req.body.createdAt = new Date();
+   topicModel.createTopic(req.body, (err, isOK) => {
+      if (err) {
+         return res.json({
+            code: 500,
+            msg: '服务器内部错误'
+         })
+      }
+      if (isOK) {
+         res.json({
+            code: 200,
+            msg: '添加话题成功'
+         })
+      } else {
+         res.json({
+            code: 400,
+            msg: '添加话题失败'
+         })
+      }
+   })
 }
 exports.showTopic = (req, res) => {
    res.send('showTopic');
